a1e7b63f7ca2df592e40cd729ed128207e47cd31
atanjaro <atanjaro@vols.utk.edu>
2025-11-25 16:12:10 -0500

    Updated arguments in test scripts.

--- test/test_example_hubbard_chain.jl
+++ test/test_example_hubbard_chain.jl
@@ -8,8 +8,8 @@
     ndn = 2
     N_equil = 2
     N_opt = 2
-    N_opt_bins=2
+    N_opt_bins = 2
     N_sim = 2
     N_sim_bins = 2
-    @test isnothing(run_hubbard_chain_simulation(sID, U, nup, ndn, N_equil, N_opt, N_opt_bins, N_sim, N_sim_bins, filepath = tempdir()))
+    @test isnothing(run_hubbard_chain_simulation(sID, L, U, nup, ndn, N_equil, N_opt, N_opt_bins, N_sim, N_sim_bins, filepath = tempdir()))
 end
