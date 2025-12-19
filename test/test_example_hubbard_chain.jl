9e33ea41f5a411d67a361d3db2aee7b692bbc314
atanjaro <atanjaro@vols.utk.edu>
2025-12-19 09:54:57 -0500

    v0.5.5-beta: Updated test scripts.

--- test/test_example_hubbard_chain.jl
+++ test/test_example_hubbard_chain.jl
@@ -1,16 +1,21 @@
 # integration test that runs a VMC simulation of a Hubbard model on a chain at half-filling
 @testitem "Hubbard Chain Example" begin
     include("../examples/hubbard_chain.jl")
-    sID = abs(rand(Int))
-    L = 4
-    U = 2.0
-    nup = 2
-    ndn = 2
-    pht = false
-    N_equil = 2
-    N_opt = 2
-    N_opt_bins = 2
-    N_sim = 2
-    N_sim_bins = 2
-    @test isnothing(run_hubbard_chain_simulation(sID, L, U, nup, ndn, pht, N_equil, N_opt, N_opt_bins, N_sim, N_sim_bins, filepath = tempdir()))
+
+    @test isnothing(
+        run_hubbard_chain_simulation(
+            sID             = abs(rand(Int)), 
+            L               = 4, 
+            U               = 2.0, 
+            nup             = 2, 
+            ndn             = 2, 
+            pht             = false, 
+            N_equil         = 2, 
+            N_opt           = 2, 
+            N_opt_bins      = 2, 
+            N_sim           = 2, 
+            N_sim_bins      = 2, 
+            filepath        = tempdir()
+        )
+    )
 end
--- test/test_example_hubbard_square.jl
+++ test/test_example_hubbard_square.jl
@@ -1,15 +1,20 @@
 # integration test that runs a VMC simulation of a Hubbard model on a square lattice at half-filling
 @testitem "Hubbard Square Example" begin
     include("../examples/hubbard_square.jl")
-    sID = abs(rand(Int))
-    L = 4
-    U = 4.0
-    density = 1.0
-    pht = false
-    N_equil = 2
-    N_opt = 2
-    N_opt_bins= 2
-    N_sim = 2
-    N_sim_bins = 2
-    @test isnothing(run_hubbard_square_simulation(sID, L, U, density, pht, N_equil, N_opt, N_opt_bins, N_sim, N_sim_bins, filepath = tempdir()))
+
+    @test isnothing(
+        run_hubbard_square_simulation(
+            sID             = abs(rand(Int)), 
+            L               = 4, 
+            U               = 4.0, 
+            density         = 1.0, 
+            pht             = false, 
+            N_equil         = 2, 
+            N_opt           = 2, 
+            N_opt_bins      = 2, 
+            N_sim           = 2, 
+            N_sim_bins      = 2, 
+            filepath        = tempdir()
+        )
+    )
 end
